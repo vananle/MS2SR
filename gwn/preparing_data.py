6d8758216f7734148aa67384d36fc327a97490fc
anle <vananle1993@gmail.com>
2021-03-22 17:39:42 +0900

    update routing

--- gwn/preparing_data.py
+++ gwn/preparing_data.py
@@ -21,3 +21,8 @@ data15 = [np.mean(data[i:i + 3], axis=0) for i in range(0, data.shape[0], 3)]
 data15 = np.asarray(data15)
 print('Abilene15', data15.shape)
 savemat('../../data/data/abilene15_tm.mat', {'X': data15})
+
+data10 = [np.mean(data[i:i + 2], axis=0) for i in range(0, data.shape[0], 2)]
+data10 = np.asarray(data10)
+print('Abilene10', data10.shape)
+savemat('../../data/data/abilene10_tm.mat', {'X': data15})
