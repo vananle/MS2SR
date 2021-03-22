75951f6bba8db6e810b4f0acc33ef1f5f8895727
anle <vananle1993@gmail.com>
2021-03-22 12:00:25 +0900

    update routing

--- gwn/preparing_data.py
+++ gwn/preparing_data.py
@@ -1,21 +1,21 @@
+import numpy as np
 from scipy.io import loadmat, savemat
 
-# dataset = 'brain_tm'
-#
-# data = loadmat('../../data/data/{}.mat'.format(dataset))['X']
-#
-# data5 = data[0:data.shape[0]:5]
-# data15 = data[0:data.shape[0]:15]
-#
-# print(data5.shape)
-# print(data15.shape)
-# savemat('../../data/data/brain5_tm.mat', {'X': data5})
+dataset = 'brain_tm'
+
+data = loadmat('../../data/data/{}.mat'.format(dataset))['X']
+
+data5 = [np.mean(data[i:i + 5], axis=0) for i in range(0, data.shape[0], 5)]
+data5 = np.asarray(data5)
+
+print('Brain5', data5.shape)
+savemat('../../data/data/brain5_tm.mat', {'X': data5})
 
 dataset = 'abilene_tm'
 
 data = loadmat('../../data/data/{}.mat'.format(dataset))['X']
 
-data15 = data[0:data.shape[0]:3]
-
-print('Abilene 15', data15.shape)
+data15 = [np.mean(data[i:i + 3], axis=0) for i in range(0, data.shape[0], 3)]
+data15 = np.asarray(data15)
+print('Abilene15', data15.shape)
 savemat('../../data/data/abilene15_tm.mat', {'X': data15})
