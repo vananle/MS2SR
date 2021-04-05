fd2d51d92063b5aa1db6cc83d05b1895c0c1eba8
vananle <vananle1993@gmail.com>
2021-04-05 18:32:10 +0900

    testint

--- gwn/utils/data.py
+++ gwn/utils/data.py
@@ -243,8 +243,8 @@ def get_dataloader(args):
     # loading data
     X = load_raw(args)
 
-    if X.shape[0] > 45000:
-        _size = 45000
+    if X.shape[0] > 10000:
+        _size = 10000
     else:
         _size = X.shape[0]
 
