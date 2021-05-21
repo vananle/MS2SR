61a6af5b28a7f8fcaa0cf0f4f75dd96cb25009f5
vananle <vananle1993@gmail.com>
2021-05-21 15:35:53 +0900

    fix dataloader missing data

--- routing/do_te.py
+++ routing/do_te.py
@@ -186,7 +186,7 @@ def ls2sr_p2(yhat, y_gt, x_gt, G, segments, te_step, args):
     solver = LS2SRSolver(G, time_limit=1, verbose=args.verbose)
 
     solution = None
-    dynamicity = np.zeros(shape=(te_step, 6))
+    dynamicity = np.zeros(shape=(te_step, 7))
     for i in range(te_step):
         mean = np.mean(y_gt[i], axis=1)
         std_mean = np.std(mean)
