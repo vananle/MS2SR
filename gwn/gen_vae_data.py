8163379941f3f13e922d1e8c92ea917bd43dc495
vananle <vananle1993@gmail.com>
2021-08-03 15:55:54 +0900

    add gen vae data

--- gwn/gen_vae_data.py
+++ gwn/gen_vae_data.py
@@ -41,7 +41,7 @@ def main(args, **model_kwargs):
     else:
         raise ValueError('Dataset not found!')
 
-    sets = ['test_2', 'test_3', 'test_4']
+    sets = ['val']
     for set in sets:
         if set == 'train' or set == 'val':
             args.test = False
