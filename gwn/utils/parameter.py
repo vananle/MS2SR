ed8a0488275082835f7d5eb005c940ceb7e57a5e
anle <vananle1993@gmail.com>
2021-09-05 19:31:42 +0900

    refactoring

--- gwn/utils/parameter.py
+++ gwn/utils/parameter.py
@@ -128,8 +128,8 @@ def get_args():
         args.kernel_size = 2
         args.stride = 2
     elif args.seq_len_y == 72:
-        args.blocks = 4
-        args.layers = 3
+        args.blocks = 5
+        args.layers = 2
         args.kernel_size = 4
         args.stride = 4
 
