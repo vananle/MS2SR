22d5bd6492a984c880b11703ff65f73dfa75c47b
anle <vananle1993@gmail.com>
2021-11-29 16:02:45 +0900

    gen vae data

--- gwn_vae/gen_vae_data.py
+++ gwn_vae/gen_vae_data.py
@@ -60,9 +60,9 @@ def main(args, **model_kwargs):
 
         train_loader, val_loader, test_loader, total_timesteps, total_series = utils.get_dataloader(args)
 
-        args.train_size, args.nSeries = train_loader.dataset.nsample, train_loader.dataset.nflows
-        args.val_size = val_loader.dataset.nsample
-        args.test_size = test_loader.dataset.nsample
+        # args.train_size, args.nSeries = train_loader.dataset.nsample, train_loader.dataset.nflows
+        # args.val_size = val_loader.dataset.nsample
+        # args.test_size = test_loader.dataset.nsample
         args.te_step = args.test_size
 
         in_dim = 1
@@ -82,7 +82,7 @@ def main(args, **model_kwargs):
 
         # engine = utils.Trainer.from_args(model, train_loader.dataset.scaler, args)
 
-        utils.print_args(args)
+        # utils.print_args(args)
 
         # Metrics on test data
         args.set = set
