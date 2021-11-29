0b5ad7b270081491a3f96fa9c958b93ac67b2d4a
anle <vananle1993@gmail.com>
2021-11-29 16:06:46 +0900

    gen vae data

--- routing/do_te.py
+++ routing/do_te.py
@@ -650,6 +650,10 @@ def srls_fix_max(max_tm, y_gt, graphs, te_step, args):
     RoutingMatrices = np.stack(RoutingMatrices, axis=0)
     TMs = np.stack(TMs, axis=0)
 
+    LinkLoads = np.reshape(LinkLoads, newshape=(-1, LinkLoads.shape[2], LinkLoads.shape[3]))
+    RoutingMatrices = np.reshape(RoutingMatrices, newshape=(-1, RoutingMatrices.shape[2], RoutingMatrices.shape[3]))
+    TMs = np.reshape(TMs, newshape=(-1, TMs.shape[2]))
+
     np.save(os.path.join(args.log_dir, 'LL_fix'), LinkLoads)
     np.save(os.path.join(args.log_dir, 'As_fix'),
             RoutingMatrices)
